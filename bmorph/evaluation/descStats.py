0b0724203e5540034aea3c22903382e649946753
steinadio <steinjao@h2o.hydro.washington.edu>
2020-02-28 14:56:57 -0800

    Color coded simple rivernetwork update using networkX, including functionality to construct a drawing all at once, or build it up from component functions. This has been added to Stat_Cor_Vis.py. Additionally, methods for normalizing and standardizing site flows have been added to descStats.py

--- bmorph/evaluation/descStats.py
+++ bmorph/evaluation/descStats.py
@@ -57,3 +57,70 @@ def pbias(observe: pd.DataFrame, predict: pd.DataFrame) -> pd.DataFrame:
     pbdf = 100*(pbdf.sum()/observe.sum())
     pbdf = pbdf.to_frame().T
     return pbdf
+
+def normalize_flow(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    normalize_flow
+        normalizes only the flow values of a given
+        DataFrame
+    ----
+    data: pd.DataFrame
+    
+    returns: pd.DataFrame of 'data' normalized without altering
+        the index
+    """
+    normal_df = pd.DataFrame(index = data.index, columns = data.columns)
+    
+    for column in data.columns:
+        column_series = data[column]
+        min_x = column_series.min()
+        max_x = column_series.max()
+        column_series = (column_series - min_x)/(max_x-min_x)
+        normal_df[column] = column_series
+    
+    return normal_df
+
+def mean_standardize_flow(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    mean_standardize_flow
+        standardize only the flow values of a given DataFrame
+        about their mean
+    ----
+    data: pd.DataFrame
+    
+    returns: pd.DataFrame of 'data' standardized about the mean
+        without alterting the index
+    """
+    mean_standard_df = pd.DataFrame(index=data.index,columns = data.columns)
+    
+    for column in data.columns:
+        column_series = data[column]
+        mean = column_series.mean()
+        std = column_series.std()
+        column_series = (column_series - mean)/std
+        mean_standard_df[column] = column_series
+    
+    return mean_standard_df
+
+def median_standardize_flow(data:pd.DataFrame) -> pd.DataFrame:
+    """
+    mean_standardize_flow
+        standardize only the flow values of a given DataFrame
+        about their median
+    ----
+    data: pd.DataFrame
+    
+    returns: pd.DataFrame of 'data' standardized about the medain
+        without alterting the index
+    """
+    
+    median_standard_df = pd.DataFrame(index = data.index, columns = data.columns)
+    
+    for column in data.columns:
+        column_series = data[column]
+        median = column_series.median()
+        mad = column_series.mad()
+        column_series = (column_series - median)/mad
+        median_standard_df[column] = column_series
+    
+    return median_standard_df
