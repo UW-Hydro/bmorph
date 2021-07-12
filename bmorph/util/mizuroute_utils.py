bbdece5723009461f1de49e0d04b9f935ec7bdcc
steinadio <steinjao@h2o.hydro.washington.edu>
2021-07-12 11:32:15 -0700

    Removed trimming dataset to intersection of timeseries and modified to_bmorph and map_var_to_segs accordingly through an xarray merge and additional parameter for the latter function

--- bmorph/util/mizuroute_utils.py
+++ bmorph/util/mizuroute_utils.py
@@ -817,7 +817,7 @@ def calculate_blend_vars(routed: xr.Dataset, topology: xr.Dataset, reference: xr
 
 
 def map_var_to_segs(routed: xr.Dataset, map_var: xr.DataArray, var_label: str,
-                    gauge_segs = None):
+                    var_key: str, gauge_segs = None):
     """
     Splits the variable into its up and down components to be used in blendmorph.
 
@@ -831,6 +831,8 @@ def map_var_to_segs(routed: xr.Dataset, map_var: xr.DataArray, var_label: str,
         the same as routed, (must also contain the dimension 'seg')
     var_label: str
         suffix of the up and down parts of the variable
+    var_key: str
+        variable name to access the variable to be split in map_var
     gauge_segs: list, optional
         List of the gauge segs that identify the reaches that are gauge sites, pulled from routed
         if None.
@@ -848,6 +850,8 @@ def map_var_to_segs(routed: xr.Dataset, map_var: xr.DataArray, var_label: str,
     map_var.load()
     routed[down_var] = np.nan * map_var
     routed[up_var] = np.nan * map_var
+    # and need to make certain dimensions all line up
+    map_var = xr.merge([map_var, routed])[var_key]
 
     for seg in routed['seg'].values:
         up_seg = routed['up_ref_seg'].sel(seg=seg)
@@ -910,7 +914,7 @@ def map_met_hru_to_seg(met_hru, topo):
 
 def to_bmorph(topo: xr.Dataset, routed: xr.Dataset, reference: xr.Dataset,
                             met_hru: xr.Dataset=None, route_var: str='IRFroutedRunoff',
-                            fill_method = 'kldiv', min_kge=None):
+                            fill_method = 'r2', min_kge=None):
 
     '''
     Prepare mizuroute output for bias correction via the blendmorph algorithm. This
@@ -923,10 +927,11 @@ def to_bmorph(topo: xr.Dataset, routed: xr.Dataset, reference: xr.Dataset,
         Topology dataset for running mizuRoute.
         We expect this to have ``seg`` and ``hru`` dimensions.
     routed: xr.Dataset
-        The initially routed dataset from mizuRoute
+        The initially routed dataset from mizuRoute.
     reference: xr.Dataset
         A dataset containing reference flows for bias correction.
-        We expect this to have ``site`` and ``time`` dimensions.
+        We expect this to have ``site`` and ``time`` dimensions with
+        flows being stored in ``reference_flow``.
     met_hru: xr.Dataset, optional
         A dataset of meteorological data to be mapped
         onto the stream segments to facilitate conditioning.
@@ -984,17 +989,16 @@ def to_bmorph(topo: xr.Dataset, routed: xr.Dataset, reference: xr.Dataset,
     met_seg = map_met_hru_to_seg(met_hru, topo)
 
     # Get the longest overlapping time period between all datasets
-    routed, reference, met_seg = trim_time([routed, reference, met_seg])
     routed = calculate_blend_vars(routed, topo, reference, route_var = route_var,
                                   fill_method = fill_method)
 
     # Put all data on segments
     seg_ref =  xr.Dataset({'reference_flow':(('time','seg'), reference['reference_flow'].values)},
                            coords = {'time': reference['time'].values, 'seg': ref_segs},)
-    routed = map_var_to_segs(routed, routed[route_var], 'raw_flow')
-    routed = map_var_to_segs(routed, seg_ref['reference_flow'], 'ref_flow')
+    routed = map_var_to_segs(routed, routed[route_var], 'raw_flow', route_var)
+    routed = map_var_to_segs(routed, seg_ref['reference_flow'], 'ref_flow', 'reference_flow')
     for v in met_vars:
-        routed = map_var_to_segs(routed, met_seg[v], v)
+        routed = map_var_to_segs(routed, met_seg[v], v, v)
 
     # Merge it all together
     met_seg = xr.merge([met_seg, routed])
